1335a73fadccc4a548d5acc7f21f6112b87dcca9
Joshua Lopez <joshualopezroque@gmail.com>
2022-06-09 20:35:22 -0700

    2 new Leetcode questions

---
+++ 183CustomersWhoNeverOrder.sql
@@ -0,0 +1,48 @@
+/* Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+Write an SQL query to report all customers who never order anything.
+
+Return the result table in any order.
+
+The query result format is in the following example.*/
+
+
+----
+
+SELECT name as Customers
+FROM customers as c
+LEFT JOIN orders as o
+ON c.id = o.customerId
+WHERE o.id IS NULL;
+
+-- also can use a sub-query and NOT IN clause
+
+SELECT customers.name AS Customers
+FROM customers
+WHERE customers.id NOT IN 
+(
+    SELECT customerID from orders
+)
